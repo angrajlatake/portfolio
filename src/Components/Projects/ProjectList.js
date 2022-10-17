c670371d7436f0e72410bf6ba003d80b3d54deb8
angrajlatake <angraj.latake@gmail.com>
2022-10-16 23:23:13 -0400

    added tasky

--- src/Components/Projects/ProjectList.js
+++ src/Components/Projects/ProjectList.js
@@ -2,10 +2,20 @@ import sudokuImg from "../../assets/mockups/sudoku.png";
 import inStock from "../../assets/mockups/InStock.png";
 import BrainFlix from "../../assets/mockups/BrainFlix.png";
 import BandSite from "../../assets/mockups/BandSite.png";
+import Tasky from "../../assets/mockups/tasky.png";
 export const projectList = [
   {
+    id: 0,
+    title: "Tasky",
+    description:
+      "Tasky is cloud-based platform that allows users to to track issues and manage project for desktop build using React and MUI. This application work with API build with Express and MongoDB database with complete authentication and authorization",
+    image: Tasky,
+    link: "https://angrajlatake-tasky.netlify.app//",
+    repo: "https://github.com/angrajlatake/Tasky",
+  },
+  {
     id: 1,
-    title: "Sudoku",
+    title: "Sudoku Solver",
     description:
       "This is a sudoku game built with React and styled with SCSS. If you are a developer, you try Depth First Search and Best First Search to solve this puzzle. If you are a user, you try to solve this puzzle.",
     image: sudokuImg,
