299bc9a3f7429b76980004c181401324913cbadf
angrajlatake <angraj.latake@gmail.com>
2023-02-26 13:34:33 -0500

    added blog page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import "./App.scss";
-import Loader from "./Components/Loader/Loader";
+import { Routes, Route } from "react-router-dom";
+
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import SmoothScroll from "./Components/Function/SmoothScroll";
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+
 import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 function App() {
-  const [loading, setLoading] = useState(true);
   return (
     <SmoothScroll>
       <div className="App">
-        <AnimatePresence>
-          {loading ? (
-            <motion.div key="loader">
-              <Loader setLoading={setLoading} />
-            </motion.div>
-          ) : (
-            <>
-              <Navbar />
-              <Home />
-              <Footer />
-            </>
-          )}
-        </AnimatePresence>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+
+        <Footer />
       </div>
     </SmoothScroll>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Projects from "../Components/Projects/Projects";
 import Stack from "../Components/Stack/Stack";
-
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import Loader from "../Components/Loader/Loader";
 const Home = () => {
+  const [loading, setLoading] = useState(true);
   return (
-    <>
-      <Hero layoutId="hero" />
-      <Stack />
-      <Projects />
-    </>
+    <AnimatePresence>
+      {loading ? (
+        <motion.div key="loader">
+          <Loader setLoading={setLoading} />
+        </motion.div>
+      ) : (
+        <>
+          <Hero layoutId="hero" />
+          <Stack />
+          <Projects />
+        </>
+      )}
+    </AnimatePresence>
   );
 };
 
